21531d977fb36fb04ca2a61189696ca8bb9720c7
ibphilippov <ibphilippov@gmail.com>
2024-08-10 18:48:08 +0500

    Terraform Sleep Fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,10 +19,17 @@ resource "google_project_service" "enabled_apis" {
   disable_on_destroy = false
 }
 
+resource "time_sleep" "wait_project_init" {
+  create_duration = "120s"
+
+  depends_on = [google_project_service.enabled_apis]
+}
+
 resource "google_storage_bucket" "data-lake-bucket" {
   name          = var.gcs_bucket_name
   location      = var.location
   force_destroy = true
+  depends_on = [time_sleep.wait_project_init]
 
 
   lifecycle_rule {
@@ -39,42 +46,47 @@ resource "random_id" "bucket_prefix" {
   byte_length = 16
 }
 
-
-#resource "google_service_account" "default" {
-#  account_id   = "test-gcf-sa"
-#  display_name = "Test Service Account"
-#}
-
 resource "google_pubsub_topic" "input" {
   name = "predictions-input"
+  depends_on = [time_sleep.wait_project_init]
+
 }
 
 resource "google_pubsub_topic" "output" {
   name = "predictions-output"
+  depends_on = [time_sleep.wait_project_init]
+
 }
 
 resource "google_storage_bucket" "function_bucket" {
   name                        = "${random_id.bucket_prefix.hex}-${var.project}-mlflow" # Every bucket name must be globally unique
   location                    = var.location
   uniform_bucket_level_access = true
+  depends_on = [time_sleep.wait_project_init]
+
 }
 
 data "archive_file" "default" {
   type        = "zip"
   output_path = "/tmp/function-source.zip"
   source_dir  = "/app/function_source/"
+  depends_on = [time_sleep.wait_project_init]
+
 }
 
 resource "google_storage_bucket_object" "function_bucket" {
   name   = "function-source.zip"
   bucket = google_storage_bucket.function_bucket.name
   source = data.archive_file.default.output_path # Path to the zipped function source code
+  depends_on = [time_sleep.wait_project_init]
+
 }
 
 resource "google_cloudfunctions2_function" "default" {
   name        = "make_prediction"
   location    = "us-central1"
   description = "function that predicts result of the match"
+  depends_on = [time_sleep.wait_project_init]
 
   build_config {
     runtime     = "python39"
@@ -113,11 +125,12 @@ resource "google_cloudfunctions2_function" "default" {
 resource "google_pubsub_subscription" "read" {
   name  = "read"
   topic = google_pubsub_topic.output.id
+  depends_on = [time_sleep.wait_project_init]
 
   labels = {
     foo = "bar"
   }
-  message_retention_duration = "120s"
+  message_retention_duration = "1200s"
   retain_acked_messages      = true
   ack_deadline_seconds = 20
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,7 +25,7 @@ variable "gcs_storage_class" {
 variable "gcp_service_list" {
   type        = list(string)
   description = "The list of apis necessary for the project"
-  default     = ["cloudfunctions.googleapis.com","pubsub.googleapis.com",
+  default     = ["serviceusage.googleapis.com","cloudresourcemanager.googleapis.com","cloudfunctions.googleapis.com","pubsub.googleapis.com",
   "eventarc.googleapis.com","logging.googleapis.com","run.googleapis.com","artifactregistry.googleapis.com",
   "cloudbuild.googleapis.com","bigquery.googleapis.com","bigquerystorage.googleapis.com",
   "iam.googleapis.com", "compute.googleapis.com","serviceusage.googleapis.com"]
